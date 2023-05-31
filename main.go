02b30de5a91196f36ae3684331a55307cc75a96b
Muhammad Mater <67539414+Micro0x00@users.noreply.github.com>
2023-05-31 15:32:12 +0300

    Create main.go
    add code

---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import"bufio"
+"fmt"
+"io/ioutil"
+"net/http"
+"os"
+"regexp"
+)
+
+func fetchCrtShDomains(domain string) ([]string, error) {
+url := fmt.Sprintf("https://crt.sh/?q=%%25.%s", domain)
+resp, err := http.Get(url)
+if err != nil {
+return nil, err
+}
+defer resp.Body.Close()
+
+body, err := ioutil.ReadAll(resp.Body)
+if err != nil {
+return nil, err
+}
+
+re := regexp.MustCompile(`<TD>\*\.([^<]+)</TD>`)
+matches := re.FindAllStringSubmatch(string(body), -1)
+
+var domains []string
+for _, := range matches {
+domains = append(domains, match[1])
+}
+
+return domains, nil
+}
+
+func main() {
+if len(os.Args) != 3 {
+fmt.Printf("Usage: %s <domainlist> <output>\n", os.Args[0])
+os.Exit(1)
+}
+
+domainlist := os.Args[1]
+output := os.Args[2]
+
+file, err := os.Open(domainlist)
+if err != nil {
+fmt.Printf("Error opening domainlist: %v\n", err)
+os.Exit(1)
+}
+defer file.Close()
+
+outFile, err := os.Create(output)
+if err != nil {
+fmt.Printf("Error creating output file: %v\n", err)
+os.Exit(1)
+}
+defer outFile.Close()
+
+writer := bufio.NewWriter(outFile)
+
+scanner := bufio.NewScanner(file)
+for scanner.Scan() {
+domain := scanner.Text()
+domains, err := fetchCrtShDomains(domain)
+if err != nil {
+fmt.Printf("Error fetching domains for %s: %v\n", domain, err)
+continue
+}
+
+for _, d := range domains {
+fmt.Println(d)
+writer.WriteString(d + "\n")
+}
+}
+
+if err := scanner.Err(); err != nil {
+fmt.Printf("Error reading domainlist: %v\n", err)
+os.Exit(1)
+}
+
+writer.Flush()
+}
